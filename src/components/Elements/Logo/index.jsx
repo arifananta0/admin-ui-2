46659dcf18a8133bad089d2c1e5054a2af808c31
Najmaaura98 <najmaaura27@gmail.com>
2024-11-01 14:13:59 +0700

    perubahan - 5

--- src/components/Elements/Logo/index.jsx
+++ src/components/Elements/Logo/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const Logo = () => {
+const Logo = (props) => {
+  const { variant = "text-primary text-4xl" } = props;
   return (
-    <div className="flex justify-center font-poppins tracking-wide text-primary text-4xl">
+    <div 
+    className={`flex justify-center font-poppins tracking-wide ${variant}`}>
           <span className="font-bold">FINE</span>bank
           <span className="font-bold">.IO</span>
     </div>
--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -1,49 +1,122 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Icon } from "../Elements/Icon";
+import Logo from "../Elements/Logo";
+
 
 const Navbar = () => {
+  const menus = [
+    {
+      id: "overview",
+      link: "/",
+      icon: <Icon.Overview />,
+      label: "Overview",
+    },
+    {
+      id: "balance",
+      link: "/balance",
+      icon: <Icon.Balance />,
+      label: "Balance",
+    },
+    {
+      id: "transaction",
+      link: "/transaction",
+      icon: <Icon.Transaction />,
+      label: "Transaction",
+    },
+    {
+      id: "bill",
+      link: "/bill",
+      icon: <Icon.Bill />,
+      label: "Bill",
+    },
+    {
+      id: "expenses",
+      link: "/expenses",
+      icon: <Icon.Expencces />,
+      label: "Expenses",
+    },
+    {
+      id: "goals",
+      link: "/goals",
+      icon: <Icon.Goals />,
+      label: "Goals",
+    },
+    {
+      id: "settings",
+      link: "/settings",
+      icon: <Icon.Settings />,
+      label: "Settings",
+    },
+  ];
+
+  const log = [
+    {
+      id: "logout",
+      link: "/logout",
+      icon: <Icon.Logout />,
+      label: "Logout",
+    },
+  ];
+
   return (
-    <nav className="bg-defaultBlack text-special-bg2 sm:w-72 w-36 min-h-screen px-7 py-12 flex flex-col justify-between ">
-        <div>
-          <Link to="/">
-          <div className="flex justify-center mb-10">Logo</div>
-          <div className="flex bg-primary text-white px-4 py-3 rounded-md">
-            <div className="text-white sm:mx-auto">A</div>
-            <div className="ms-3 text-white hidden sm:block">Overview</div>
+    <nav className="bg-defaultBlack text-special-bg2 sm:w-72 w-28 min-h-screen px-7 py-12 flex flex-col justify-between">
+      <div>
+        <div className="flex justify-center mb-10">
+          <Logo variant="text-white text-sm sm:text-2xl"/>
           </div>
-          </Link>
+        {menus.map((menu) => (
+          <NavLink
+          to={menu.link} 
+          key={menu.id}
+          className={({ isActive }) => 
+            isActive ? "flex bg-primary text-white px-4 py-3 rounded-md" : "flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md"
+          }
+          >
+  
+              <div className="mx-auto sm:mx-0">{menu.icon}</div>
+              <div className="ms-3 hidden sm:block">{menu.label}</div>
+            
+          </NavLink>
+        ))}
+      </div>
 
-          <Link to="/balance">
-          <div className="flex bg-primary text-white px-4 py-3 rounded-md">
-            <div className="text-white sm:mx-auto">B</div>
-            <div className="ms-3 text-white hidden sm:block">Balance</div>
+      <div className="sticky bottom-12">
+        {log.map((item) => (
+          <NavLink 
+          to={item.link} 
+          key={item.id}
+          className={({ isActive }) => 
+            isActive ? "flex bg-primary text-white px-4 py-3 rounded-md" : "flex bg-special-bg3 px-4 py-3 rounded-md hover:text-white"
+          }
+          >
+          
+            <div className="mx-auto sm:mx-0">{item.icon}</div>
+            <div className="ms-3 hidden sm:block">{item.label}</div>
+          
+        </NavLink>
+        ))}
+        
+        <div className="border-b my-10 border-b-special-bg"></div>
+        <div className="flex justify-between">
+          <div className="mx-auto sm:mx-0">
+            <img src="src/images/profile-1.png" />
           </div>
-          </Link>
 
-          <Link to="/">
-          <div className="flex bg-primary text-white px-4 py-3 rounded-md">
-            <div className="text-white sm:mx-auto">C</div>
-            <div className="ms-3 text-white hidden sm:block">Transaction</div>
-          </div>
-          </Link>
-        </div>
-        <div>
-          <div className="flex bg-special-bg3 px-4 py-3 rounded-md">
-            <div className="mx-auto sm:mx-auto">D</div>
-            <div className="ms-3 text-special-bg2 hidden sm:block">Logout</div>
-          </div>
-          <div className="border-b my-10 border-b-special-bg"></div>
-          <div className="flex justify-between">
-            <div className="mx-auto sm:mx-auto">foto</div>
-            <div className="hidden sm:block">
+          <div className="hidden sm:block">
+            <div className="text-white font-bold">
               Username
-              <br />
+            </div>
+            <div className="text-sx">
               View Profile
             </div>
-            <div className="hidden sm:block">icon</div>
           </div>
+          <div className="hidden sm:block self-center justify-self-end">
+            <Icon.ThreeMenu />
+            </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 };
 
